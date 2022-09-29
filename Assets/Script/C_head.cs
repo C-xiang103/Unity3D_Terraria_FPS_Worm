dbdd98824717cabdcff4b2fc2165eacd0ddd0d2b
wangchaoxiang <wangchaoxiang@tal.com>
2022-09-29 16:52:54 +0800

    头部脚本

--- Assets/Script/C_head.cs
+++ Assets/Script/C_head.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class C_head : MonoBehaviour
 {
+    private Transform head;
+    private float go_x, go_y, go_z;
+    private Vector3 run;
+    private float wait_time;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        head = this.transform;
+        run = new Vector3(go_x, go_y, go_z);
+        wait_time = 0f;
+        speed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        head.position += run * Time.deltaTime * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        if(wait_time>=0)
+        {
+            wait_time-=Time.fixedDeltaTime;
+            return;
+        }
+        wait_time = Random.Range(2f, 8f);
+        //speed = Random.Range(1f, 10f);
+        run = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
 }
