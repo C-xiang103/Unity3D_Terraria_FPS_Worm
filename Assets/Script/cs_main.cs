98862c9d627c3cd4e3111561a4f6386cfa0c8eb0
wangchaoxiang <wangchaoxiang@tal.com>
2022-09-29 15:44:32 +0800

    3秒更改一次旋转频率

--- Assets/Script/cs_main.cs
+++ Assets/Script/cs_main.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class cs_main : MonoBehaviour
 {
-    public int speed=10;
+    public float speed;
+    private float rotatemax = 500f;
+    private float rotatemin = 10f;
+    private float booltime;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 30;
+        speed = rotatemax;
+        booltime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(booltime<=3)
+        {
+            booltime += Time.deltaTime;
+        }
+        else
+        {
+            booltime = 0;
+            speed = Random.Range(rotatemin, rotatemax);
+        }
         this.transform.Rotate(0,Time.deltaTime*speed,0);
-    }
-
-    private void FixedUpdate()
-    {
-
     }
 }
